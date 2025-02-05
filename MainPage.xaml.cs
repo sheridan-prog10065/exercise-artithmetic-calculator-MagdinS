59dc4e54e8401afbd73c314cb7fb39179e3535ff
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2025-02-05 10:23:58 -0500

    Implement the logic of the application. Perform calculations using various operators and display the artihmetic operation.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace MathOperators;
+﻿using System.Diagnostics;
+
+namespace MathOperators;
 
 public partial class MainPage : ContentPage
 {
@@ -7,4 +9,84 @@ public partial class MainPage : ContentPage
         InitializeComponent();
     }
 
+    private void OnCalculate(object sender, EventArgs e)
+    {
+        //Get the input to the arithmetic operation
+        double leftOperand = double.Parse(_txtLeftOp.Text);
+        double rightOperand = double.Parse(_txtRightOp.Text);
+        
+        //Obtain the character that represents the arithmetic operation
+        //Cast to string is possible because SelectedItem is an Object
+        //Extra paranthesis are needed to ensure the index operator is applied to the result of the cast
+        char operation = ((string)_pckOperand.SelectedItem)[0]; 
+
+        //Perform the arithmetic operation and obtain the result
+        double result = PerformArithmeticOperation(operation, leftOperand, rightOperand);
+
+        //Display the arithmetic calculation to the user. Show the work!
+        _txtMathExp.Text = $"{leftOperand} {operation} {rightOperand} = {result}";
+    }
+
+    private double PerformArithmeticOperation(char operation, double leftOperand, double rightOperand)
+    {
+        switch (operation)
+        {
+            case '+':
+                return PerformAddition(leftOperand, rightOperand);
+
+            case '-':
+                return PerformSubraction(leftOperand, rightOperand);
+
+            case '*':
+                return PerformMultiplication(leftOperand, rightOperand);
+
+            case '/':
+                return PerformDivision(leftOperand, rightOperand);
+
+            case '%':
+                return PerformDivRemainder(leftOperand, rightOperand);
+
+            default:
+                Debug.Assert(false, "Unknown arithmetic opearand. Cannot perform the artighmetic operation.");
+                return 0;
+        }
+    }
+
+    private double PerformAddition(double leftOperand, double rightOperand)
+    {
+        return (leftOperand + rightOperand);
+    }
+    
+    private double PerformSubraction(double leftOperand, double rightOperand)
+    {
+        return leftOperand - rightOperand;
+    }
+    
+    private double PerformMultiplication(double leftOperand, double rightOperand)
+    {
+        return leftOperand * rightOperand;
+    }
+    
+    private double PerformDivision(double leftOperand, double rightOperand)
+    {
+        string divOp = _pckOperand.SelectedItem as string; //another way of casting used for objects
+        if (divOp.Contains("Int", StringComparison.OrdinalIgnoreCase))
+        {
+            //Integer division is performed when the operands are both integers
+            int intLeftOp = (int)leftOperand;
+            int intRightOp = (int)rightOperand;
+            double result =  intLeftOp / intRightOp;
+            return result;
+        }
+        else
+        {
+            //Real division
+            return leftOperand / rightOperand;    
+        }
+    }
+    
+    private double PerformDivRemainder(double leftOperand, double rightOperand)
+    {                                                                          
+        return leftOperand % rightOperand;                                     
+    }                                                                          
 }
